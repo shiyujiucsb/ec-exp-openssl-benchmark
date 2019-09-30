b89afe4b3e34b764129b6a209c0fedd0efdc052b
Shiyu Ji <shiyu@cs.ucsb.edu>
2019-09-29 17:27:56 -0700

    Init code; update readme file.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ec-exp-openssl-benchmark
-Benchmark for massive elliptic curve exponential computations using openssl.
+Benchmark for massive exponential computations using openssl.
+
+Use Ubuntu. Install openssl-dev
+``
+sudo apt-get install openssl-dev
+``
+
+The compilation instructions are in ``build.sh``.
+
---
+++ test.cpp
@@ -0,0 +1,153 @@
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+#include <vector>
+
+typedef unsigned char byte;
+
+constexpr int N_BITS = 512;
+
+EC_GROUP *InitializeCurve() {
+  EC_GROUP *curve;
+
+  if ((curve = EC_GROUP_new_by_curve_name(NID_secp224r1)) == nullptr) {
+    std::cerr << "ERROR: Curve init failed." << std::endl;
+    return nullptr;
+  }
+
+  return curve;
+}
+
+std::vector<BIGNUM*> GenRandomBigNums(const int num_iters) {
+  std::vector<BIGNUM*> results(num_iters);
+
+  for (int i = 0; i < num_iters; i++) {
+    results[i] = BN_new();
+    BN_rand(results[i], N_BITS, BN_RAND_TOP_ANY, BN_RAND_BOTTOM_ANY);
+  }
+
+  return results;
+}
+
+/* Straightforward method for EC computation. */
+std::vector<EC_POINT*> BruteForce(
+		const EC_GROUP* const curve,
+		const std::vector<BIGNUM*>& exponents,
+		BN_CTX* ctx) {
+  const int N = exponents.size();
+  std::vector<EC_POINT*> results;
+  results.reserve(N);
+
+  for (int i = 0; i < N; i++) {
+    EC_POINT* res = EC_POINT_new(curve);
+    assert(EC_POINT_mul(curve, res, exponents[i], nullptr, nullptr, ctx) == 1);
+    results.emplace_back(res);
+  }
+
+  return results;
+}
+
+/* Initialize precomputing results in a 2D table. */
+std::vector<std::vector<EC_POINT*>> InitializeDpTable(
+		const EC_GROUP* const curve,
+		BN_CTX* ctx) {
+  const int n_bytes = N_BITS / 8;
+  const int n_combs = (int(1) << 8);
+  byte* exponent = new byte[n_bytes];
+
+  std::vector<std::vector<EC_POINT*>> dp(n_bytes,
+		  std::vector<EC_POINT*>(n_combs));
+
+  for (int i = 0; i < n_bytes; i++) {
+    std::memset(exponent, 0, n_bytes);
+    for (int j = 0; j < n_combs; j++) {
+      exponent[i] = byte(j);
+      dp[i][j] = EC_POINT_new(curve);
+      assert(EC_POINT_mul(curve, dp[i][j],
+                 BN_bin2bn(exponent, n_bytes, nullptr),
+		 nullptr, nullptr, ctx) == 1);
+    }
+  }
+
+  delete[] exponent;
+  return dp;
+}
+
+/* EC computation using precomputed results. */
+std::vector<EC_POINT*> DpMethod(
+		const EC_GROUP* const curve,
+		const std::vector<BIGNUM*>& exponents,
+		const std::vector<std::vector<EC_POINT*>>& dp,
+		BN_CTX* ctx) {
+  const int N = exponents.size();
+  std::vector<EC_POINT*> results;
+  results.reserve(N);
+
+  const int n_bytes = N_BITS / 8;
+  byte* exponent = new byte[n_bytes];
+  for (int i = 0; i < N; i++) {
+    const int len_used = BN_bn2bin(exponents[i], exponent);  // big-endian
+    assert(len_used <= n_bytes);
+    EC_POINT* res = EC_POINT_new(curve);
+    assert(EC_POINT_set_to_infinity(curve, res) == 1);
+    for (int j = 0; j < len_used; j++) {
+      const int k = j + n_bytes - len_used;
+      assert(EC_POINT_add(curve, res, res, dp[k][exponent[j]], ctx) == 1);
+    }
+    results.emplace_back(res);
+  }
+
+  delete[] exponent;
+  return results;
+}
+
+/* Compare results computed by two methods as above. */
+bool BigNumVectorCmp(const EC_GROUP* const curve,
+		     const std::vector<EC_POINT*>& v,
+		     const std::vector<EC_POINT*>& u,
+		     BN_CTX* ctx) {
+  if (v.size() != u.size()) {
+    return false;
+  }
+  for (int i = 0; i < v.size(); i++) {
+    if (EC_POINT_cmp(curve, v[i], u[i], ctx) != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int main() {
+  const EC_GROUP* curve = InitializeCurve();
+  /* Number of iterations for testing */
+  const int num_iters = 10000;
+
+  BN_CTX *ctx = BN_CTX_new();
+  std::vector<BIGNUM*> exponents = GenRandomBigNums(num_iters);
+
+  auto bf_start = std::chrono::high_resolution_clock::now();
+  std::vector<EC_POINT*> bf_results = BruteForce(curve, exponents, ctx);
+  auto bf_end = std::chrono::high_resolution_clock::now();
+
+  std::vector<std::vector<EC_POINT*>> dp = InitializeDpTable(curve, ctx);
+
+  auto dp_start = std::chrono::high_resolution_clock::now();
+  std::vector<EC_POINT*> dp_results = DpMethod(curve, exponents, dp, ctx);
+  auto dp_end = std::chrono::high_resolution_clock::now();
+
+  assert(BigNumVectorCmp(curve, bf_results, dp_results, ctx));
+
+  std::cout << "BF: " <<
+	  std::chrono::duration_cast<std::chrono::duration<double>>(
+			  bf_end - bf_start).count() << " sec." << std::endl;
+  std::cout << "DP: " <<
+	  std::chrono::duration_cast<std::chrono::duration<double>>(
+			  dp_end - dp_start).count() << " sec." << std::endl;
+
+  return 0;
+}
+
